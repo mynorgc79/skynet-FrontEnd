110dc6df4f95edf3a64c628d0b20e74709918190
mynorgc79 <mynorguarchaj99@gmail.com>
2025-10-19 09:39:32 -0600

    feat: definir rutas+path alliance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    // canActivate: [publicGuard],
+    loadChildren: () => import('@features/public/public.module').then((m) => m.PublicModule),
+  },
+  {
+    path: 'dashboard',
+    // canActivate: [privateGuard],
+    loadChildren: () => import('@features/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: '404',
+    loadComponent: () =>
+      import('@shared/pages/index').then((c) => c.NotFoundComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
