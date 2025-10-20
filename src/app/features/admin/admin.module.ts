05d8fbfa0978246efadeae31836cb9315a57c3a0
mynorgc79 <mynorguarchaj99@gmail.com>
2025-10-20 15:19:47 -0600

    agregar componentes

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AdminRoutingModule } from './admin-routing.module';
 
-
+// Components
+import { DashboardLayoutComponent } from './pages/dashboard-layout.component';
+import { DashboardComponent } from './pages/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardLayoutComponent,
+    DashboardComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModule { }
