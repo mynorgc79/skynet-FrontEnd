66135788b3da5c883f7e62b7c5a2c9b1b9fa9b98
mynorgc79 <mynorguarchaj99@gmail.com>
2025-10-24 00:33:44 -0600

    ajuste envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,16 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.skynet.com/api', // Cambiar por URL de producción
   authEndpoints: {
-    login: '/auth/login/',
+    login: '/usuarios/login/',
     refresh: '/auth/refresh/',
     logout: '/auth/logout/',
     profile: '/auth/profile/'
+  },
+  userEndpoints: {
+    create: '/usuarios/usuarios/create/',
+    list: '/usuarios/usuarios/',
+    detail: '/usuarios/usuarios/', // + id/
+    update: '/usuarios/usuarios/', // + id/
+    delete: '/usuarios/usuarios/'  // + id/
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,16 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api',
   authEndpoints: {
-    login: '/auth/login/',
+    login: '/usuarios/login/',
     refresh: '/auth/refresh/',
     logout: '/auth/logout/',
     profile: '/auth/profile/'
+  },
+  userEndpoints: {
+    create: '/usuarios/usuarios/create/',
+    list: '/usuarios/usuarios/',
+    detail: '/usuarios/usuarios/', // + id/
+    update: '/usuarios/usuarios/', // + id/
+    delete: '/usuarios/usuarios/'  // + id/
   }
 };
